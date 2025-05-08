65e1dbcb957d062f9a6ff2075aa6238f3f2e75ef
HughesTT <100058635+HughesTT@users.noreply.github.com>
2025-05-08 11:41:32 +0800

    0508_update_add mobicouponcode

--- src/components/CouponView.vue
+++ src/components/CouponView.vue
@@ -14,19 +14,56 @@
       </div>
     </div>
   </div>
+  <div class="homecoupon-mobi">
+    <div class="homecoupon-box">
+      <div class="homecoupon-text">母親節優惠碼</div>
+      <div class="homecoupon-code">
+        ILOVEMOM
+      </div>
+      <!-- 使用 v-show 控制顯示與隱藏 -->
+      <div class="homecoupon-codecopy" v-show="copycheck">
+        <i class="bi bi-check"></i>複製成功
+      </div>
+      <div class="homecoupon-codecopy" @click="copyCouponcode" v-show="!copycheck">
+        <i class="bi bi-copy"></i> 點我複製
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
 .homecoupon {
   background: url(../assets/img/couponbg.png) no-repeat center center;
   width: 100%;
   height: 400px;
+  display: block;
+
+  @media(max-width:960px) {
+    display: none;
+  }
+}
+
+.homecoupon-mobi {
+  background: url(../assets/img/mobi_couponcode.png) no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 250px;
+  display: none;
+
+  @media(max-width:960px) {
+    display: block;
+  }
 }
 
 .homecoupon-box {
   position: relative;
   top: 70%;
   margin-left: 30%;
+
+  @media(max-width:960px) {
+    top: 60%;
+    margin-left: 7%;
+  }
 }
 
 .homecoupon-text {
@@ -34,6 +71,10 @@
   font-size: 1.3em;
   font-weight: bold;
   margin-bottom: 5px;
+
+  @media(max-width:960px) {
+    font-size: 1em;
+  }
 }
 
 .homecoupon-code {
@@ -43,7 +84,15 @@
   border-radius: 10px;
   text-align: center;
   font-size: 1.3em;
+  font-weight: bold;
+  color: rgb(211, 0, 0);
   box-shadow: 4px 4px 8px rgba(20%, 20%, 40%, 0.5);
+
+  @media(max-width:960px) {
+    width: 150px;
+    padding: 5px 1em;
+    font-size: 1em;
+  }
 }
 
 .homecoupon-codecopy {
